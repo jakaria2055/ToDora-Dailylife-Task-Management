e62abc695451b83c4a31606119372ee86b23d3da
Jakaria Ahmed <jakaria88520@gmail.com>
2025-09-16 21:17:49 +0600

    cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true
+}));
 
 
 
