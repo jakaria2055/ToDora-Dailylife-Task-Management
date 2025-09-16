afb6ff727e611fe6213030bf32e5781f4e152053
Jakaria Ahmed <jakaria88520@gmail.com>
2025-09-16 20:46:26 +0600

    test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-import path from "path";
+// import path from "path";
 import cors from 'cors'; 
 import router from './routes/api.js';
 import cookieParser from 'cookie-parser';
@@ -19,11 +19,11 @@ app.use(cors());
 app.use('/api/v1', router)
 
 
-app.use(express.static(path.join('../ToDo-Client/dist')));
+// app.use(express.static(path.join('../ToDo-Client/dist')));
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(clientDistPath,'ToDo-Client','index.html','index.html'));
-})
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(clientDistPath,'ToDo-Client','index.html','index.html'));
+// })
 
 
 
