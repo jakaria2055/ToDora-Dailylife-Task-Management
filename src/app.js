b9812068121aa2c4d288b3d51419ede98bd1ef93
Jakaria Ahmed <jakaria88520@gmail.com>
2025-09-16 22:01:34 +0600

    cors

--- src/app.js
+++ src/app.js
@@ -4,30 +4,13 @@ import cors from 'cors';
 import router from './routes/api.js';
 import cookieParser from 'cookie-parser';
 
-const allowedOrigins = [
-  "http://localhost:5173", // your local frontend
-  "https://to-dora-dailylife-task-management.vercel.app" // your deployed frontend
-];
-
 const app = express();
 
 // MIDDLEWARES
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: (origin, callback) => {
-    console.log("CORS request origin:", origin);
-    if (!origin) return callback(null, true); // allow server-to-server or Postman
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // ✅ important for cookies
-}));
-// app.use(cors());
+app.use(cors());
 
 
 // ROUTES ENTRY
